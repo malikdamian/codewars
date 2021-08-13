396c7a503b6a39481b560e977a6bf88477ffe9c4
Damian Malik <malikdamiann@gmail.com>
2021-08-13 19:31:30 +0200

    Create Simple Encryption #1 - Alternating Split.py

---
+++ 6kyu/Simple Encryption #1 - Alternating Split.py
@@ -0,0 +1,31 @@
+"""Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed
+characters of S with all the even-indexed characters of S, this process should be repeated N times.
+
+Examples:
+
+encrypt("012345", 1)  =>  "135024"
+encrypt("012345", 2)  =>  "135024"  ->  "304152"
+encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+
+encrypt("01234", 1)  =>  "13024"
+encrypt("01234", 2)  =>  "13024"  ->  "32104"
+encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+Together with the encryption function, you should also implement a decryption function which reverses the process.
+
+If the string S is an empty value or the integer N is not positive, return the first argument without changes."""
+
+
+def decrypt(encrypted_text, n):
+    for _ in range(n):
+        mid = len(encrypted_text) // 2
+        even = encrypted_text[:mid]
+        odd = encrypted_text[mid:]
+        encrypted_text = ''.join(odd[i:i + 1] + even[i:i + 1] for i in range(mid + 1))
+
+    return encrypted_text
+
+
+def encrypt(text, n):
+    for _ in range(n):
+        text = text[1::2] + text[::2]
+    return text
