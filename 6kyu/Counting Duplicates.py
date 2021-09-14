c8e8e2217befd9e22dc1333272bb2a789692b829
Damian Malik <malikdamiann@gmail.com>
2021-09-14 14:15:12 +0200

    Create Counting Duplicates.py

---
+++ 6kyu/Counting Duplicates.py
@@ -0,0 +1,13 @@
+"""Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice"""
+
+
+def duplicate_count(text):
+    return len([char for char in set(text.lower()) if text.lower().count(char) > 1])
+
