ada8f93236b3fc8eab81519f1880de3cb815f822
Damian Malik <malikdamiann@gmail.com>
2021-10-10 11:53:15 +0200

    Create Number of trailing zeros of N!.py

---
+++ 5kyu/Number of trailing zeros of N!.py
@@ -0,0 +1,23 @@
+"""Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+N! = 1 * 2 * 3 * ... * N
+
+Be careful 1000! has 2568 digits...
+
+For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+Examples
+zeros(6) = 1
+# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+zeros(12) = 2
+# 12! = 479001600 --> 2 trailing zeros"""
+
+
+def zeros(n):
+    count = 0
+
+    while n >= 5:
+        n = n // 5
+        count += n
+    return count
